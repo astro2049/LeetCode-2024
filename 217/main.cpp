3578398cca43efa49bf15d2a3c16bc09b16778fb
Jerry Lyu <45759373+astro2049@users.noreply.github.com>
2024-10-30 14:09:11 +0000

    217. Contains Duplicate

---
+++ 217/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
+
+class Solution {
+public:
+    bool containsDuplicate(vector<int> &nums) {
+        sort(nums.begin(), nums.end());
+        for (int i = 0; i < nums.size() - 1; i++) {
+            if (nums[i] == nums[i + 1]) {
+                return true;
+            }
+        }
+        return false;
+    }
+};
