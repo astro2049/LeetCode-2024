5669c6902e58ccd21c71c7ac0fea0d7640aeb410
Jerry Lyu <45759373+astro2049@users.noreply.github.com>
2024-11-03 04:57:54 +0000

    53. Maximum Subarray

---
+++ 53/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
+
+class Solution {
+public:
+    int maxSubArray(vector<int> &nums) {
+        int maxSum = INT_MIN;
+        int sum = 0;
+        for (int &num: nums) {
+            sum += num;
+            maxSum = max(maxSum, sum);
+            if (sum < 0) {
+                sum = 0;
+            }
+        }
+        return maxSum;
+    }
+};
