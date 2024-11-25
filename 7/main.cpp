a84a2d4b7b5e1bc35157978b7725a1396ddb351d
Jerry Lyu <45759373+astro2049@users.noreply.github.com>
2024-11-25 21:01:36 +0000

    7. Reverse Integer

---
+++ 7/main.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
+
+class Solution {
+public:
+    int reverse(int x) {
+        string s = to_string(x);
+        if (x >= 0) {
+            for (int i = 0, j = s.length() - 1; i < j; i++, j--) {
+                swap(s[i], s[j]);
+            }
+
+            string maxx = to_string(INT_MAX);
+            if (s.length() < maxx.length()) {
+                return stoi(s);
+            } else {
+                for (int i = 0; i < s.length(); i++) {
+                    if (s[i] > maxx[i]) {
+                        return 0;
+                    } else if (s[i] < maxx[i]) {
+                        break;
+                    }
+                }
+                return stoi(s);
+            }
+        } else {
+            for (int i = 1, j = s.length() - 1; i < j; i++, j--) {
+                swap(s[i], s[j]);
+            }
+
+            string minn = to_string(INT_MIN);
+            if (s.length() < minn.length()) {
+                return stoi(s);
+            } else {
+                for (int i = 1; i < s.length(); i++) {
+                    if (s[i] > minn[i]) {
+                        return 0;
+                    } else if (s[i] < minn[i]) {
+                        break;
+                    }
+                }
+                return stoi(s);
+            }
+        }
+    }
+};
