9c807d413b2375df7969d1d8fd095de26ea89502
Jerry Lyu <45759373+astro2049@users.noreply.github.com>
2024-12-08 23:58:39 +0000

    300. Longest Increasing Subsequence

---
+++ 300/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
+
+class Solution {
+public:
+    int lengthOfLIS(vector<int> &nums) {
+        int maxLen = -1;
+
+        vector<int> dp(nums.size(), 1);
+        for (int i = 0; i < nums.size(); i++) {
+            for (int j = 0; j < i; j++) {
+                if (nums[j] < nums[i]) {
+                    dp[i] = max(dp[i], dp[j] + 1);
+                }
+            }
+            maxLen = max(maxLen, dp[i]);
+        }
+
+        return maxLen;
+    }
+};
